74520308e68902e8acfd86991f980bbd40603ab2
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-05-26 23:37:01 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const { Logger } = require("mongodb");
+const connectDB = require("./config/connectDB");
+const Task = require("./models/taskModel");
+const taskRoutes = require("./routes/taskRoute");
+
+const productRoutes = require("./routes/productRoute")
+const app = express();
+const cors = require('cors');
+
+// Middleware
+app.use(express.json()); // To use Postman data in console [ need to use json type in postman]
+app.use(express.urlencoded({ extended: false })); // To use Postman data in console [ need to use url encoded (Body >> url encoded) type in postman]
+
+app.use(cors({
+  origin: [
+  "http://localhost:3000", 
+  "https://mern-task-app-2ql0.onrender.com" 
+]
+}));
+// app.use(taskRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/products", productRoutes);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async (arg) => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
