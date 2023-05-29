55f65b9ce979306dfaad671987d23decb92371ef
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-05-29 21:56:40 +0530

    CHG | update productCOntroller.test.js

--- backend/tests/productController.test.js
+++ backend/tests/productController.test.js
@@ -1,101 +1,134 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../server');
+const mongoose = require('mongoose');
 const Product = require('../models/productModel');
 
 describe('Product Controller', () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
 
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  beforeEach(async () => {
     await Product.deleteMany();
   });
 
-  describe('POST /api/products', () => {
-    test('should create a new product', async () => {
-      const productData = { productName: 'Test Product', category: 'Test Category' };
+  it('should create a new product', async () => {
+    const productData = {
+      productName: 'Test Product',
+      category: 'Test Category',
+    };
 
-      const response = await request(app)
-        .post('/api/products')
-        .send(productData)
-        .expect(200);
+    const response = await request(app)
+      .post('/api/products/post-product')
+      .send(productData);
 
-      expect(response.body.productName).toBe('Test Product');
-      expect(response.body.category).toBe('Test Category');
-    });
+    expect(response.status).toBe(200);
+    expect(response.body.productName).toBe(productData.productName);
+    expect(response.body.category).toBe(productData.category);
   });
 
-  describe('GET /api/products', () => {
-    test('should get all products', async () => {
-      await Product.create({ productName: 'Product 1', category: 'Category 1' });
-      await Product.create({ productName: 'Product 2', category: 'Category 2' });
-
-      const response = await request(app).get('/api/products').expect(200);
-
-      expect(response.body.length).toBe(2);
-      expect(response.body[0].productName).toBe('Product 1');
-      expect(response.body[0].category).toBe('Category 1');
-      expect(response.body[1].productName).toBe('Product 2');
-      expect(response.body[1].category).toBe('Category 2');
+  it('should get all products', async () => {
+    await Product.create({
+      productName: 'Product 1',
+      category: 'Category 1',
     });
+    await Product.create({
+      productName: 'Product 2',
+      category: 'Category 2',
+    });
+
+    const response = await request(app).get('/api/products/get-products');
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(2);
   });
 
-  describe('GET /products/:id', () => {
-    it('should return a single product if it exists', async () => {
-      const response = await request(app).get('/products/1');
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('productName');
-      expect(response.body).toHaveProperty('category');
+  it('should return a single product if it exists', async () => {
+    const product = await Product.create({
+      productName: 'Test Product',
+      category: 'Test Category',
     });
 
-    it('should return 404 if product does not exist', async () => {
-      const response = await request(app).get('/products/100');
-      expect(response.status).toBe(404);
-      expect(response.body).toEqual('No Product with id: 100');
-    });
+    const response = await request(app).get(`/api/products/get-product/${product._id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body.productName).toBe(product.productName);
+    expect(response.body.category).toBe(product.category);
   });
 
-  describe('PUT /products/:id', () => {
-    it('should update a product if it exists', async () => {
-      const updatedProductData = {
+  it('should return 404 if product does not exist', async () => {
+    const invalidProductId = 'invalidProductId';
+
+    const response = await request(app).get(`/api/products/get-product/${invalidProductId}`);
+
+    expect(response.status).toBe(404);
+    expect(response.body).toBe(`No Product with id: ${invalidProductId}`);
+  });
+
+  it('should update a product if it exists', async () => {
+    const product = await Product.create({
+      productName: 'Test Product',
+      category: 'Test Category',
+    });
+
+    const updatedProductData = {
+      productName: 'Updated Product',
+      category: 'Updated Category',
+    };
+
+    const response = await request(app)
+      .put(`/api/products/update-product/${product._id}`)
+      .send(updatedProductData);
+
+    expect(response.status).toBe(200);
+    expect(response.body.productName).toBe(updatedProductData.productName);
+    expect(response.body.category).toBe(updatedProductData.category);
+  });
+
+  it('should return 404 if product does not exist (update)', async () => {
+    const invalidProductId = 'invalidProductId';
+
+    const response = await request(app)
+      .put(`/api/products/update-product/${invalidProductId}`)
+      .send({
         productName: 'Updated Product',
         category: 'Updated Category',
-      };
+      });
 
-      const response = await request(app)
-        .put('/products/1')
-        .send(updatedProductData);
-
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('productName', updatedProductData.productName);
-      expect(response.body).toHaveProperty('category', updatedProductData.category);
-    });
-
-    it('should return 404 if product does not exist', async () => {
-      const updatedProductData = {
-        productName: 'Updated Product',
-        category: 'Updated Category',
-      };
-
-      const response = await request(app)
-        .put('/products/100')
-        .send(updatedProductData);
-
-      expect(response.status).toBe(404);
-      expect(response.body).toEqual('No Product with id: 100');
-    });
+    expect(response.status).toBe(404);
+    expect(response.body).toBe(`No Product with id: ${invalidProductId}`);
   });
 
-  describe('DELETE /products/:id', () => {
-    it('should delete a product if it exists', async () => {
-      const response = await request(app).delete('/products/1');
-      expect(response.status).toBe(200);
-      expect(response.text).toBe('Product Deleted');
+
+  it('should delete a product if it exists', async () => {
+    const product = await Product.create({
+      productName: 'Test Product',
+      category: 'Test Category',
     });
 
-    it('should return 404 if product does not exist', async () => {
-      const response = await request(app).delete('/products/100');
-      expect(response.status).toBe(404);
-      expect(response.body).toEqual('No Product with id: 100');
-    });
-  }); 
-  
-  // Add more test cases for other controller functions (e.g., getProduct, updateProduct, deleteProduct)
+    const response = await request(app).delete(`/api/products/delete-product/${product._id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Product Deleted');
+
+    const deletedProduct = await Product.findById(product._id);
+    expect(deletedProduct).toBeNull();
+  });
+
+  it('should return 404 if product does not exist', async () => {
+    const invalidProductId = 'invalidProductId';
+
+    const response = await request(app).delete(`/api/products/delete-product/${invalidProductId}`);
+
+    expect(response.status).toBe(404);
+    expect(response.body).toBe(`No Product with id: ${invalidProductId}`);
+  });
+
 });
